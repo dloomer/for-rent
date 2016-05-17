cffd6b4f99a558b8a0b375e6656bb50a55e33e65
Dave Loomer <david.loomer@target.com>
2016-05-16 22:29:27 -0500

    Mail stuff

--- backend/web/views/task_handlers.py
+++ backend/web/views/task_handlers.py
@@ -11,6 +11,7 @@ import webapp2
 from app.lib.data_connectors import feed_config_connector
 from app.lib.services.feed_item_parsers import CraigslistFeedItem
 from app.lib.data_connectors.core_objects import PropertyListing
+import app.lib.services.mail as mail
 
 class DataFeedEntriesHandler(webapp2.RequestHandler):
     def get(self):
@@ -26,32 +27,17 @@ class DataFeedEntriesHandler(webapp2.RequestHandler):
             if not property_listing:
                 continue
             if property_listing.is_new:
-                # fork into inbox items
-                pass
+                mail.send_property_notification(property_listing.db_object, item_url)
             elif property_listing.is_dirty:
                 # update existing inbox items
                 pass
-                # 
 
 '''
-from app.lib.data_connectors import feed_config_connector
-from app.lib.services.feed_item_parsers import CraigslistFeedItem
-from app.lib.data_connectors.core_objects import PropertyListing
+import app.lib.services.mail as mail
+import app.models.core as core_models
 
-feed_name = 'downtown-seattle'
-item_urls = ['http://seattle.craigslist.org/see/apa/5584557553.html']
-
-feeds_map = feed_config_connector.get_feeds_map()
-feed = feeds_map[feed_name]
-
-for item_url in item_urls:
-    feed_item = CraigslistFeedItem(item_url, feed)
-    property_listing = PropertyListing.from_parsed_feed_item(feed_item)
-    if property_listing.is_new:
-        # fork into inbox items
-        pass
-    elif property_listing.is_dirty:
-        # update existing inbox items
-        pass
-        # 
+item_url = "http://www.google.com"
+property_listing = core_models.PropertyListing.get_by_key_name('47.6057333|-122.3161506')
+item_url = [item.item_link for item in property_listing.feeditemcache_set()][0]
+mail.send_property_notification(property_listing, item_url)
 '''
