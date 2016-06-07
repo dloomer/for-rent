467e607f2d89f6722dc0a4b9d35f46a1cf8f2a58
Dave Loomer <david.loomer@target.com>
2016-06-07 13:34:06 -0700

    Pass cached feed data to parsers where appropriate

--- backend/web/views/task_handlers.py
+++ backend/web/views/task_handlers.py
@@ -5,13 +5,14 @@
 """
 
 # related third party imports
+import json
 import webapp2
 import logging
 
 # local application/library specific imports
 from app.lib.data_connectors import feed_config_connector
 from app.lib.services.feed_item_parsers import FeedItem
-from app.lib.data_connectors.core_objects import PropertyListing, feed_item_metadata
+from app.lib.data_connectors.core_objects import PropertyListing, feed_item_from_url
 import app.lib.services.mail as mail
 
 class DataFeedEntriesHandler(webapp2.RequestHandler):
@@ -22,12 +23,32 @@ class DataFeedEntriesHandler(webapp2.RequestHandler):
         feeds_map = feed_config_connector.get_feeds_map()
         feed = feeds_map[feed_name]
 
+        _source_types = feed_config_connector.get_source_types()
+        use_feed_metadata = _source_types[feed['source_type']].get('use_feed_metadata')
+
         for item_url in item_urls:
-            if feed['source_type'] in ['zillow']:
-                cached_metadata = feed_item_metadata(feed_name, item_url)
-            else:
-                cached_metadata = None
-            feed_item = FeedItem.from_feed(feed, item_url, cached_metadata=cached_metadata)
+            db_feed_item = feed_item_from_url(feed_name, item_url)
+            cached_metadata = db_feed_item.cached_metadata
+
+            db_property_listing = db_feed_item.property_listing \
+                if db_feed_item.property_listing else None
+            cached_location_data = {
+                'geo': [db_property_listing.geo.lat, db_property_listing.geo.lon],
+                'address': db_property_listing.address,
+                'city': db_property_listing.city,
+                'postal_code': db_property_listing.postal_code,
+                'neighborhood': db_property_listing.neighborhood,
+                'state_code': db_property_listing.state_code,
+                'country_code': db_property_listing.country_code
+            } if db_property_listing else None
+            fetched_data = cached_metadata if use_feed_metadata else None
+            feed_item = FeedItem.from_feed(
+                feed,
+                item_url,
+                fetched_data=fetched_data,
+                cached_metadata=cached_metadata,
+                cached_location_data=cached_location_data
+            )
             feed_item.parse()
             try:
                 property_listing = PropertyListing.from_parsed_feed_item(feed_item)
